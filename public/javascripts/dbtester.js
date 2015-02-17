bd8abb41bc2f9917d46a1367c9800528a8c87553
franklingu <franklingujunchao@gmail.com>
2015-02-17 15:52:01 +0800

    completed and tested isBCNF
    getAllKeys is the key to solve 2NF and 3NF

--- public/javascripts/dbtester.js
+++ public/javascripts/dbtester.js
@@ -1,2 +1,30 @@
+// return whether a relation is 2NF
+// @attrs: []; @fds: [{left: '', right; '', type:''}]
+function is2NF(attrs, fds) {
+  var attrSet = removeDuplicates(attrs);
+  var fdSet = removeDuplicates(fds);
+  return false;  // stuck now. wait on findAllKeys to be completed
+}
 
+// return whether a relation is 3NF
+// @attrs: []; @fds: [{left: '', right; '', type:''}]
+function is3NF(attrs, fds) {
+  var attrSet = removeDuplicates(attrs);
+  var fdSet = removeDuplicates(fds);
+  return false;  // stuck now. wait on findAllKeys to be completed
+}
 
+// return whether a relation is BCNF
+// @attrs: []; @fds: [{left: '', right; '', type:''}]
+function isBCNF(attrs, fds) {
+  var attrSet = removeDuplicates(attrs);
+  var fdSet = removeDuplicates(fds);
+  var isInBCNF = true;
+  _.forEach(fdSet, function (item) {
+    if (!(isSubset(item.left, item.right)
+      || isSuperkeyForRelation(item.left, attrSet, fdSet))) {
+      isInBCNF = false;
+    }
+  });
+  return isInBCNF;
+}
---
+++ tests/test-dbtester.js
@@ -0,0 +1,87 @@
+QUnit.test('is2NF test', function (assert) {
+  assert.ok(false, 'Not implemented');
+});
+
+QUnit.test('is3NF test', function (assert) {
+  assert.ok(false, 'Not implemented');
+});
+
+QUnit.test('isBCNF test', function (assert) {
+  var attrs = ['A', 'B', 'C', 'D', 'E'];
+  var fds = [{
+    left: ['A', 'B', 'C'],
+    right: ['C', 'D'],
+    type: 'fd'
+  }, {
+    left: ['A', 'B'],
+    right: ['E', 'C'],
+    type: 'fd'
+  }];
+  assert.ok(isBCNF(attrs, fds), 'In BCNF');
+
+  fds = [{
+    left: ['A', 'B'],
+    right: ['C', 'D'],
+    type: 'fd'
+  }, {
+    left: ['A', 'B'],
+    right: ['E'],
+    type: 'fd'
+  }];
+  assert.ok(isBCNF(attrs, fds), 'In BCNF');
+
+  fds = [{
+    left: ['A', 'B'],
+    right: ['C', 'D'],
+    type: 'fd'
+  }, {
+    left: ['A', 'B', 'D'],
+    right: ['E'],
+    type: 'fd'
+  }];
+  assert.ok(isBCNF(attrs, fds), 'In BCNF');
+
+  fds = [{
+    left: ['A', 'B'],
+    right: ['C', 'D'],
+    type: 'fd'
+  }, {
+    left: ['A'],
+    right: ['E'],
+    type: 'fd'
+  }];
+  assert.ok(!isBCNF(attrs, fds), 'In BCNF');
+
+  fds = [{
+    left: ['A'],
+    right: ['B'],
+    type: 'fd'
+  }, {
+    left: ['B', 'C'],
+    right: ['E'],
+    type: 'fd'
+  }, {
+    left: ['D', 'E'],
+    right: ['A'],
+    type: 'fd'
+  }];
+  assert.ok(!isBCNF(attrs, fds), 'Not BCNF');
+
+  fds = [{
+    left: ['A', 'B'],
+    right: ['C', 'D'],
+    type: 'fd'
+  }],
+  assert.ok(!isBCNF(attrs, fds), 'Not BCNF');
+
+  fds = [{
+    left: ['A', 'B'],
+    right: ['D'],
+    type: 'fd'
+  }, {
+    left: ['A', 'B', 'C'],
+    right: ['E'],
+    type: 'fd'
+  }],
+  assert.ok(!isBCNF(attrs, fds), 'Not BCNF');
+});
